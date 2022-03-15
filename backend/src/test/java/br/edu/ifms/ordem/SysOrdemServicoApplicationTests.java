318b053b9fc2fe5a35cd8f86a34a0d75bca29fa0
MarcosRogerioFerreiraIFMS <marcos.rogerio@ifms.edu.br>
2022-03-15 19:34:10 -0300

    Criando o projeto

---
+++ backend/src/test/java/br/edu/ifms/ordem/SysOrdemServicoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifms.ordem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SysOrdemServicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
