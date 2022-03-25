9d6b1981d5566da14b60211d9e1c2caab8700d83
MarcosRogerioFerreiraIFMS <marcos.rogerio@ifms.edu.br>
2022-03-24 21:42:06 -0300

    Seeding no Banco de Dados

---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO TB_TECNICO (nome, telefone, email, senha) VALUES ('Marcos Rogerio', '86876876', 'marcos@gmail.com', '1234');
+INSERT INTO TB_TECNICO (nome, telefone, email, senha) VALUES ('Bruno Mazeo', '86876876', 'bruno@gmail.com', '1234');
+INSERT INTO TB_TECNICO (nome, telefone, email, senha) VALUES ('Claudia Raia', '86876876', 'claudia@gmail.com', '1234');
